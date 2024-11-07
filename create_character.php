0b6e7a1cbec354d99eb9e6adf43ff61a7cc4884b
Manel <manelmoralesrivera@gmail.com>
2024-11-07 13:03:29 +0100

    Creacion de personaje v2

---
+++ create_character.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crea tu personaje</title>
+</head>
+<body>
+    <h1>Crea tu personaje</h1>
+    <form></form>
+</body>
+</html>
